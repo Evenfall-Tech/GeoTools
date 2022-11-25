a40f42ed2be4078a2b0c2f7b7e9dea5db5c3d893
Mykola Morozov <mcmikecreations@gmail.com>
2022-11-25 23:53:12 +0900

    Ran code cleanup.

--- src/GeoTools.GeoServer/Services/WorkspaceService.cs
+++ src/GeoTools.GeoServer/Services/WorkspaceService.cs
@@ -6,16 +6,13 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace GeoTools.GeoServer.Services
 {
--- tests/GeoTools.GeoServer.Tests/Usings.cs
+++ tests/GeoTools.GeoServer.Tests/Usings.cs
@@ -1,4 +1,4 @@
+global using GeoTools.GeoServer.Models;
+global using GeoTools.GeoServer.Services;
 global using Xunit;
 global using Xunit.Abstractions;
-global using GeoTools.GeoServer.Services;
-global using GeoTools.GeoServer.Models;
